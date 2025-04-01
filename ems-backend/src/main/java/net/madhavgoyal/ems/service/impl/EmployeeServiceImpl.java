998ef9480c834bcd81a0cb80ac5619227d5fb115
Madhav Goyal <madhavgoyal012@gmail.com>
2025-04-01 19:11:19 +0530

    EMS

---
+++ ems-backend/src/main/java/net/madhavgoyal/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,69 @@
+package net.madhavgoyal.ems.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.madhavgoyal.ems.dto.EmployeeDto;
+import net.madhavgoyal.ems.entity.Employee;
+import net.madhavgoyal.ems.exception.ResourceNotFoundException;
+import net.madhavgoyal.ems.mapper.EmployeeMapper;
+import net.madhavgoyal.ems.repository.EmployeeRepository;
+import net.madhavgoyal.ems.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.maptoEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.maptoEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exists with given Id : " + employeeId ));
+
+        return EmployeeMapper.maptoEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.maptoEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                ()-> new ResourceNotFoundException("Employee is not exists with given Id : " + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.maptoEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                ()-> new ResourceNotFoundException("Employee is not exists with given Id : " + employeeId)
+        );
+
+        employeeRepository.deleteById(employeeId);
+    }
+
+
+}
---
+++ ems-frontend/src/Components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p>Copyright &copy; Employee Management System </p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
